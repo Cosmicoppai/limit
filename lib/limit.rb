372820025c2570cf121b1938a4854a05cff8425a
CosmicOppai <cosmicoppai@protonmail.com>
2025-04-29 23:11:21 +0530

    feat: rate-limiter gem

---
+++ lib/limit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "limit/version"
+
+module Limit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_limit.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestLimit < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Limit::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
